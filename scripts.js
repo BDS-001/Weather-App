efd0c62bc5d0f199c73162dac6c4a3c0785a6b9e
BDS <113209967+BDS-001@users.noreply.github.com>
2024-01-26 21:53:24 -0500

    Add function to fetch weather data using API in event listener

--- scripts.js
+++ scripts.js
@@ -1,2 +1,13 @@
+document.getElementById('weatherForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const location = document.getElementById('locationInput').value;
+    const apiKey = document.getElementById('apiKeyInput').value;
 
-`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${query}`
+    async function getweatherData(apiKey, location) {
+        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`, {mode: 'cors'})
+        const weatherData = await response.json()
+        console.log(weatherData)
+    }
+
+    getweatherData(apiKey, location)
+});
