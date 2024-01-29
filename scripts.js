3c1d57eeb4b0115e08947a5591faf3c59a21db96
BDS <113209967+BDS-001@users.noreply.github.com>
2024-01-28 21:33:36 -0500

    grab city and forcast for 3 days in console

--- scripts.js
+++ scripts.js
@@ -1,13 +1,17 @@
+async function getweatherData(apiKey, location) {
+    const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=3`, {mode: 'cors'})
+    const weatherData = await response.json()
+    
+    console.log(weatherData)
+    console.log(weatherData['location']['name'])
+    console.log(weatherData['forecast']['forecastday'][0])
+    console.log(weatherData['forecast']['forecastday'][1])
+    console.log(weatherData['forecast']['forecastday'][2])
+}
+
 document.getElementById('weatherForm').addEventListener('submit', function(event) {
     event.preventDefault();
     const location = document.getElementById('locationInput').value;
     const apiKey = document.getElementById('apiKeyInput').value;
-
-    async function getweatherData(apiKey, location) {
-        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`, {mode: 'cors'})
-        const weatherData = await response.json()
-        console.log(weatherData)
-    }
-
     getweatherData(apiKey, location)
 });
